c5ee0ddde1a9e3cd50436246caf4658d09da1b42
PhilippRo <rosendahl@otris.de>
2017-08-02 15:03:10 +0200

    Add support for JSRDBG.

--- main.cpp
+++ main.cpp
@@ -10,12 +10,20 @@
 #pragma warning(disable : 4127 4100 4512 4800 4267 4251)
 #endif
 
+// spidermonkey
 #include <jsapi.h>
 
+// jsrdbg
+#include <jsrdbg/jsrdbg.h>
+
+// standard libraries
+#include <atomic>
 #include <cstdlib>
 #include <fstream>
 #include <iostream>
+#include <map>
 #include <mutex>
+#include <stdexcept>
 #include <string>
 #include <thread>
 #include <vector>
@@ -88,19 +96,35 @@ void reportError (JSContext* ctx, const char* message, JSErrorReport* report) {
                 << message << std::endl;
 }
 
+class JSRunnerException : public std::runtime_error {
+    public:
+        JSRunnerException(const std::string& mss)
+                : std::runtime_error(mss) {
+        }
+
+        using std::runtime_error::what;
+};
+
 class JSRunner {
 
 protected:
     JSRuntime *rt;
     JSContext *cx;
+
+    static JSR::JSRemoteDebugger* debugger;
+    static std::atomic_int debugger_ref_cnt;
+
     std::string source;
     std::string path;
 
 public:
 
     JSRunner(std::string p_path) {
+        // ensures, that no jsrunners are created simultainiously, because that will cause a crahs
         static std::mutex on_create_mtx;
         std::lock_guard<std::mutex> lock(on_create_mtx);
+
+        debugger_ref_cnt++;
 
         path = p_path;
         // load source
@@ -113,28 +137,29 @@ public:
             source.assign((std::istreambuf_iterator<char>(ifstr)), std::istreambuf_iterator<char>());
         }
 
-        // init JS engine
-        //    JS_Init
-        //if ( !JS_Init() ) {
-        //    throw "unable to init engine";
-        //}
-        std::cout << "creating a new runtime" << std::endl;
+        // set up spidermonkey
         rt = JS_NewRuntime(8L * 1024L * 1024L, JS_USE_HELPER_THREADS);
-        std::cout << "created a new runtime" << std::endl;
         if ( !rt ) {
-            throw "unable to create a runtime";
+            throw JSRunnerException("unable to create a runtime");
         }
 
         cx = JS_NewContext(rt, 8192);
         if ( !cx ) {
-            throw "unable to create a context";
+            throw JSRunnerException("unable to create a context");
         }
         JS_SetErrorReporter(cx, &reportError);
         
+        // set stack size
+        const std::size_t max_stack_size = 128 * sizeof(std::size_t) * 1024;
+        JS_SetNativeStackQuota(rt, max_stack_size);
+
+        initDbg();
     }
 
     ~JSRunner() {
+
         if ( !cx ) {
+            debugger->uninstall(cx);
             JS_DestroyContext(cx);
         }
 
@@ -142,6 +167,62 @@ public:
             JS_DestroyRuntime(rt);
         }
 
+        debugger_ref_cnt--;
+        if( debugger_ref_cnt == 0 && !debugger ) {
+            debugger->stop();
+            delete debugger;
+        }
+
+    }
+
+    void initDbg() {
+       static  class script_loader: public JSR::IJSScriptLoader{
+            protected:
+            std::map<JSContext*, std::string> sources;
+            std::mutex mtx;
+
+            public:
+            void register_source(JSContext* ctx, std::string source) {
+                std::lock_guard<std::mutex> locker(mtx);
+                sources[ctx] = source;
+            }
+
+            int load( JSContext *ctx, const std::string &path, std::string &script ) {
+                std::lock_guard<std::mutex> locker(mtx);
+                try {
+                    script = sources[ctx];
+                    return JSR_ERROR_NO_ERROR;
+                } catch (std::out_of_range& e) {
+                    return JSR_ERROR_FILE_NOT_FOUND;
+                }
+            }
+        } loader;
+
+        loader.register_source(cx, source);
+
+        bool needs_start = false;
+        if ( !debugger ) { 
+            JSR::JSRemoteDebuggerCfg dbgCfg;
+            dbgCfg.setTcpHost(JSR_DEFAULT_TCP_BINDING_IP);
+            dbgCfg.setTcpPort(JSR_DEFAULT_TCP_PORT);
+            dbgCfg.setScriptLoader(new script_loader());
+
+            debugger = new JSR::JSRemoteDebugger(dbgCfg);
+            needs_start = true;
+        }
+
+        JSR::JSDbgEngineOptions dbgOptions;
+        dbgOptions.suspended();
+        if( debugger->install( cx, path, dbgOptions ) != JSR_ERROR_NO_ERROR ) {
+            throw JSRunnerException("Cannot install debugger");
+        }
+
+        if(needs_start) {
+            if ( debugger->start() != JSR_ERROR_NO_ERROR) {
+                debugger->uninstall(cx);
+                throw JSRunnerException("Cannot start debugger");
+            }
+        }
     }
 
     // runs the script
@@ -153,28 +234,33 @@ public:
         compartment_options.setVersion(JSVERSION_LATEST);
         global = JS_NewGlobalObject(cx, &globalClass, nullptr,  compartment_options);
         if ( !global ) {
-            throw "could not create a global object";
+            throw JSRunnerException("could not create a global object");
         }
 
         JSAutoCompartment ac(cx, global);
 
         if ( !JS_InitStandardClasses(cx, global) ) {
-            throw "unable to init standard classes";
+            throw JSRunnerException("unable to init standard classes");
         }
 
         if ( !JS_DefineFunctions(cx, global, globalFunctions) ) {
-            throw "unable to define functions";
+            throw JSRunnerException("unable to define functions");
+        }
+
+        if ( debugger->addDebuggee( cx, global ) != JSR_ERROR_NO_ERROR ) {
+            throw JSRunnerException("unable to add debuggee");
         }
 
         jsval *some = nullptr;
         if ( !JS_EvaluateScript(cx, global, source.c_str(), source.size(), path.c_str(), 0, some) ) {
-            throw "could not exeute script";
+            throw JSRunnerException("could not exeute script");
         }
     }
 
 
 };
-
+JSR::JSRemoteDebugger* JSRunner::debugger;
+std::atomic_int JSRunner::debugger_ref_cnt(0);
 
 int main(int argc, char** argv) {
     if (argc < 2) {
@@ -191,6 +277,7 @@ int main(int argc, char** argv) {
                     std::cout << "an error occured: " << c << std::endl; 
                 } 
             }));
+            std::cout << "started js instance " << i << std::endl; 
         }
         for(auto& thr : threads) {
             thr.join();
